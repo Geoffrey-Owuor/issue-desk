b3fcfd6d9e6c78610f1ac124a49bc54170e742ba
Geoffrey-Owuor <geoffreyowuor22@gmail.com>
2026-02-02 17:00:51 +0300

    Initial metadata updates

--- app/(DashBoardRoutes)/dashboard/layout.tsx
+++ app/(DashBoardRoutes)/dashboard/layout.tsx
@@ -2,6 +2,23 @@ import { requireSession } from "@/lib/Auth";
 import { redirect } from "next/navigation";
 import { UserProvider } from "@/contexts/UserContext";
 import DashboardLayoutShell from "@/components/Navigation/DashboardLayoutShell";
+import { Metadata } from "next";
+
+export const generateMetadata = async (): Promise<Metadata> => {
+  const user = await requireSession();
+
+  if (!user) return { title: "Dashboard" };
+
+  const role = user.role.charAt(0).toUpperCase() + user.role.slice(1);
+
+  return {
+    title: {
+      default: `${user.username}'s Dashboard - ${role}`,
+      template: `%s - ${user.username}'s Dashboard`,
+    },
+    description: "The default user dashboard page",
+  };
+};
 
 const layout = async ({ children }: { children: React.ReactNode }) => {
   // Check Auth and redirect if there is no valid user
